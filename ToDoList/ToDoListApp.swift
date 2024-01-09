2ee0ac76d2401a31ce3b8bcc4538f3575db33cb7
mohamedEldeeb13 <mooel744@gmail.com>
2024-01-09 17:49:39 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Mohamed Abd Elhakam on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
